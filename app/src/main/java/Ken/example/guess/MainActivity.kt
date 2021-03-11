f0c3cdf3c5d1b6b5ad13902d1b6f69bd1ce76de1
USER <a1821010@yahoo.com.tw>
2021-03-11 10:30:27 +0800

    function design, toast, dialog

--- app/src/main/java/Ken/example/guess/MainActivity.kt
+++ app/src/main/java/Ken/example/guess/MainActivity.kt
@@ -2,12 +2,43 @@ package Ken.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        R.string.ok
+        Log.d("test", "secret : " + secretNumber.secret)
     }
+
+    fun check(view : View){
+        val n = ed_number.text.toString().toInt()
+        println("number : $n")
+        Log.d("test","num : " + n)
+        val diff = secretNumber.validate(n)
+        var msg = "yesUGotIt !"
+        if(secretNumber.validate(n) < 0){
+            msg = "BIGGER";
+//            Toast.makeText(this, "BIGGER", Toast.LENGTH_LONG).show()
+        }else if(diff > 0){
+            msg = "smaller";
+//            Toast.makeText(this, "smaller", Toast.LENGTH_LONG).show()
+        }else{
+//            Toast.makeText(this, "命中 ! ", Toast.LENGTH_LONG).show()
+        }
+//        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(msg)
+            .setPositiveButton("OK", null)
+            .show()
+    }
+
 }
 
---
+++ app/src/main/java/Ken/example/guess/secretNumber.kt
@@ -0,0 +1,21 @@
+package Ken.example.guess
+
+import java.util.*
+
+class SecretNumber {
+    val secret: Int = Random().nextInt(10) + 1
+    var count = 0
+    fun validate(number: Int) : Int {
+        count++
+        return number - secret
+    }
+}
+
+
+fun main(){
+    val secretNumber = SecretNumber()
+    println(secretNumber.secret)
+    //println(secretNumber.validate(2))
+    println("${println(secretNumber.validate(2))}, " +
+            "count: ${secretNumber.count}")
+}
